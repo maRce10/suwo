3ca1faf6092123c5d85a94be780d7f083365681f
Jorge Elizondo-Calvo <111101802+Jorge-Elizondo-C@users.noreply.github.com>
2023-07-31 11:15:14 -0600

    Added description for gbif_dataset_update

--- tests/testthat/gbif_dataset_update.R
+++ tests/testthat/gbif_dataset_update.R
@@ -1,9 +1,35 @@
-#install.packages("httr")
-#install.packages("readr")
-library(httr)
-library(readr)
+#' Access 'gbif' dataset information
+#'
+#' \code{gbif_dataset_update} searches for dataset information from \href{https://www.gbif.org}{gbif}.
+#' @usage gbif_dataset_update()
+#' @return returns dataset csv from gbif
+#' @export
+#' @name gbif_dataset_update
+#' @details This function queries for dataset info in the open-access
+#' online repository \href{https://www.gbif.org}{gbif}.
+#' @examples
+#' \dontrun{
+#' # update dataset information
+# df1 <- gbif_dataset_update()
+#' View(df1)
+#'
+#' }
+#'
+#' @references {
+#'
+#' }
+#' @author Marcelo Araya-Salas (\email{marcelo.araya@@ucr.ac.cr})
+#'
 
 update_gbif_datasets <- function(gbif_datasets) {
+
+  #check internet connection
+  a <- try(RCurl::getURL("https://www.gbif.org"), silent = TRUE)
+  if (is(a, "try-error"))
+    stop2("No connection to gbif (check your internet connection!)")
+
+  if (a == "Could not connect to the database")
+
   # Download the CSV file
   response <- GET("https://api.gbif.org/v1/dataset/search/export?format=CSV&")
 
