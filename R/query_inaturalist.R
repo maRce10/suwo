71db30cc333b67abe1507ee090413a47be6cb67a
Jorge Elizondo-Calvo <111101802+Jorge-Elizondo-C@users.noreply.github.com>
2025-03-25 00:18:03 -0600

    Added Query_Inaturalist offset query search
    Query_inaturalist now searches using offset instead of changing pages. This allows full download of all metadata as before it was capped at a set number of pages

--- R/query_inaturalist.R
+++ R/query_inaturalist.R
@@ -36,234 +36,156 @@
 #' @author Marcelo Araya-Salas (\email{marcelo.araya@@ucr.ac.cr})
 #'
 
-query_inaturalist <-
-  function(term,
-           cores = getOption("mc.cores", 1),
-           pb = getOption("pb", TRUE),
-           verbose = getOption("verbose", TRUE),
-           type = c("sound", "still image"),
-           identified = FALSE,
-           verifiable = FALSE,
-           all_data = getOption("all_data", TRUE)) {
-    # check arguments
-    arguments <- as.list(base::match.call())[-1]
+query_inaturalist <- function(term,
+                              cores = getOption("mc.cores", 1),
+                              pb = getOption("pb", TRUE),
+                              verbose = getOption("verbose", TRUE),
+                              type = c("sound", "still image"),
+                              identified = FALSE,
+                              verifiable = FALSE,
+                              all_data = getOption("all_data", TRUE)) {
 
-    # add objects to argument names
-    for (i in names(arguments)) {
-      arguments[[i]] <- get(i)
+  arguments <- as.list(base::match.call())[-1]
+
+  for (i in names(arguments)) {
+    arguments[[i]] <- get(i)
+  }
+
+  check_results <- .check_arguments(args = arguments)
+  checkmate::reportAssertions(check_results)
+
+  org_type <- type <- rlang::arg_match(type)
+  type <- switch(type, sound = "sounds", "still image" = "photos")
+
+  response <- try(httr::GET("https://www.inaturalist.org/"), silent = TRUE)
+  if (inherits(response, "try-error") || httr::http_error(response)) {
+    .stop("No connection to INaturalist (check your internet connection!)")
+  }
+
+  content <- httr::content(response, as = "text")
+  if (grepl("Could not connect to the database", content)) {
+    .stop("INaturalist website is apparently down")
+  }
+
+  species <- term
+  term <- gsub(" ", "%20", term)
+
+  base_url <- paste0(
+    "https://api.inaturalist.org/v1/observations?per_page=200&",
+    "taxon_name=", term, "&",
+    type, "=true", "&", "identified=",
+    identified, "&", "verifiable=", verifiable
+  )
+
+  first_query <- jsonlite::fromJSON(base_url)
+  total_results <- first_query$total_results
+  if (total_results == 0) {
+    if (verbose) {
+      cat(paste(.color_text(paste0("No ", tolower(org_type), "s were found"), "failure"), .add_emoji("sad")))
+    }
+    return(data.frame())
+  } else {
+    if (pb & verbose) {
+      cat(paste(.color_text(paste0("Obtaining metadata (", total_results, " matching observation(s) found)"), "success"), .add_emoji("happy"), ":\n"))
     }
 
-    # check each arguments
-    check_results <- .check_arguments(args = arguments)
+    offsets <- seq(0, total_results, by = 200)
 
-    # report errors
-    checkmate::reportAssertions(check_results)
-
-    # assign a value to type
-    org_type <- type <- rlang::arg_match(type)
-
-    type <- switch(type,
-                   sound = "sounds",
-                   "still image" = "photos"
-    )
-
-    # Check internet connection using httr and error handling
-    response <- try(httr::GET("https://www.inaturalist.org/"), silent = TRUE)
-    if (inherits(response, "try-error") || httr::http_error(response)) {
-      .stop("No connection to INaturalist (check your internet connection!)")
-    }
-
-    content <- httr::content(response, as = "text")
-    if (grepl("Could not connect to the database", content)) {
-      .stop("INaturalist website is apparently down")
-    }
-
-    # Save species name
-    species <- term
-
-    # format JSON
-    term <- gsub(" ", "%20", term)
-
-    srch_trm <- paste0(
-      "https://api.inaturalist.org/v1/observations?per_page=200&",
-      "taxon_name=", term, "&",
-      type, "=true", "&", "identified=",
-      identified, "&", "verifiable=", verifiable
-    )
-
-    base.srch.pth <- jsonlite::fromJSON(srch_trm)
-
-    #### org_type in tolower
-
-    # message if nothing found
-    if (base.srch.pth$total_results == 0) {
-      if (verbose) {
-        cat(paste(.color_text(paste0("No ", tolower(org_type), "s were found"), "failure"), .add_emoji("sad")))
-      }
+    if (Sys.info()[1] == "Windows" & cores > 1) {
+      cl <- parallel::makePSOCKcluster(getOption("cl.cores", cores))
     } else {
-      # message number of results
-      if (pb & verbose) {
-        cat(paste(.color_text(paste0("Obtaining metadata (", base.srch.pth$total_results, " matching observation(s) found)"), "success"), .add_emoji("happy"), ":\n"))
+      cl <- cores
+    }
+
+    query_output_list <- pblapply_sw_int(offsets, cl = cl, pbar = pb, function(offset) {
+      query_output <- jsonlite::fromJSON(paste0(base_url, "&offset=", offset))
+
+      if (is.null(query_output$results)) {
+        return(NULL)
       }
 
+      query_output$results <- lapply(seq_len(nrow(query_output$results)), function(u) {
+        x <- as.data.frame(query_output$results[u, ])
+        media_df <- if (type == "sounds") do.call(rbind, x$sounds) else do.call(rbind, x$photos)
+        media_df <- media_df[!sapply(media_df, is.list)]
+        media_df <- data.frame(media_df)
+        names(media_df)[names(media_df) == "url"] <- "media-URL"
 
-      # get total number of pages
-      pages <- (seq_len(ceiling(base.srch.pth$total_results / base.srch.pth$per_page)))
-
-      # set clusters for windows OS
-      if (Sys.info()[1] == "Windows" & cores > 1) {
-        cl <- parallel::makePSOCKcluster(getOption("cl.cores", cores))
-      } else {
-        cl <- cores
-      }
-
-
-      query_output_list <- pblapply_sw_int(pages, cl = cl, pbar = pb, function(i) {
-        query_output <- jsonlite::fromJSON(paste0(srch_trm, "&page=", i))
-
-        # format as list of data frame
-        query_output$results <- lapply(seq_len(nrow(query_output$results)), function(u) {
-          x <- as.data.frame(query_output$results[u, ])
-
-          if (type == "sounds") {
-            media_df <- do.call(rbind, x$sounds)
-          } else {
-            media_df <- do.call(rbind, x$photos)
-          }
-          # media data frame with image details
-          media_df <- media_df[!sapply(media_df, is.list)]
-          media_df <- data.frame(media_df)
-          names(media_df)[names(media_df) == "url"] <- "media-URL"
-
-          # remove lists
-          x <- x[!sapply(x, is.list)]
-
-          # make it data frame
-          X_df <- data.frame(t(unlist(x)))
-
-          # add media details
-          X_df <- cbind(X_df, media_df)
-
-          return(X_df)
-        })
-
-        # get common names to all data frames in X
-        common_names <- unique(unlist(lapply(query_output$results, names)))
-
-        # add missing columns to all data frames in X
-        query_output$results <- lapply(query_output$results, function(e) {
-          nms <- names(e)
-          if (length(nms) != length(common_names)) {
-            for (o in common_names[!common_names %in% nms]) {
-              e <-
-                data.frame(e,
-                           NA,
-                           stringsAsFactors = FALSE,
-                           check.names = FALSE
-                )
-              names(e)[ncol(e)] <- o
-            }
-          }
-          return(e)
-        })
-
-        # all results in a single data frame
-        output_df <- do.call(rbind, query_output$results)
-
-        output_df$page <- i
-
-        return(output_df)
+        x <- x[!sapply(x, is.list)]
+        X_df <- data.frame(t(unlist(x)))
+        X_df <- cbind(X_df, media_df)
+        return(X_df)
       })
 
-      # get common names to all data frames in X
-      common_names <- unique(unlist(lapply(query_output_list, names)))
-
-      # add missing columns to all data frames in X
-      query_output_list <- lapply(query_output_list, function(e) {
+      common_names <- unique(unlist(lapply(query_output$results, names)))
+      query_output$results <- lapply(query_output$results, function(e) {
         nms <- names(e)
         if (length(nms) != length(common_names)) {
           for (o in common_names[!common_names %in% nms]) {
-            e <-
-              data.frame(e,
-                         NA,
-                         stringsAsFactors = FALSE,
-                         check.names = FALSE
-              )
+            e <- data.frame(e, NA, stringsAsFactors = FALSE, check.names = FALSE)
             names(e)[ncol(e)] <- o
           }
         }
         return(e)
       })
 
-      # all results in a single data frame
-      query_output_df <- do.call(rbind, query_output_list)
+      output_df <- do.call(rbind, query_output$results)
+      output_df$offset <- offset
+      return(output_df)
+    })
 
-      # Change column name for media download function
-      colnames(query_output_df)[colnames(query_output_df) == "media-URL"] <- "file_url"
-
-      # Add repository ID
-      query_output_df$repository <- "INAT"
-
-      split_location <- do.call(rbind, strsplit(as.character(query_output_df$location), ","))
-
-      # Convert the split values to numeric
-      latitude <- split_location[, 1]
-      longitude <- split_location[, 2]
-
-      # Add the latitude and longitude columns to the data frame
-      query_output_df$latitude <- latitude
-      query_output_df$longitude <- longitude
-
-
-      # Find the index of the first occurrence of "id" in the old column names
-      first_id_index <- which(names(query_output_df) == "id")[1]
-
-      # Check if a match was found before proceeding
-      if (!is.na(first_id_index)) {
-        # Replace the first occurrence of "id" with "key" in the new column names
-        names(query_output_df)[first_id_index] <- "key"
+    common_names <- unique(unlist(lapply(query_output_list, names)))
+    query_output_list <- lapply(query_output_list, function(e) {
+      nms <- names(e)
+      if (length(nms) != length(common_names)) {
+        for (o in common_names[!common_names %in% nms]) {
+          e <- data.frame(e, NA, stringsAsFactors = FALSE, check.names = FALSE)
+          names(e)[ncol(e)] <- o
+        }
       }
+      return(e)
+    })
 
+    query_output_df <- do.call(rbind, query_output_list)
+    colnames(query_output_df)[colnames(query_output_df) == "media-URL"] <- "file_url"
+    query_output_df$repository <- "INAT"
 
-      # rename output columns
-      names_df <- data.frame(old = c("quality_grade", "time_observed_at", "taxon_geoprivacy", "uuid", "key", "cached_votes_total", "identifications_most_agree", "species_guess", "identifications_most_disagree", "positional_accuracy", "comments_count", "site_id", "created_time_zone", "license_code", "observed_time_zone", "public_positional_accuracy", "oauth_application_id", "created_at", "description", "time_zone_offset", "observed_on", "observed_on_string", "updated_at", "captive", "faves_count", "num_identification_agreements", "map_scale", "uri", "community_taxon_id", "owners_identification_from_vision", "identifications_count", "obscured", "num_identification_disagreements", "geoprivacy", "location", "spam", "mappable", "identifications_some_agree", "place_guess", "file_url", "subtype", "play_local", "native_sound_id", "attribution", "id", "file_content_type", "license_code", "secret_token", "hidden", "page", "repository","latitude", "longitude"), new = c("quality_grade", "time_observed_at", "taxon_geoprivacy", "uuid", "key", "cached_votes_total", "identifications_most_agree", "species_guess", "identifications_most_disagree", "positional_accuracy", "comments_count", "site_id", "created_time_zone", "license_code", "observed_time_zone", "public_positional_accuracy", "oauth_application_id", "created_at", "description", "time_zone_offset", "observed_on", "observed_on_string", "updated_at", "captive", "faves_count", "num_identification_agreements", "map_scale", "uri", "community_taxon_id", "owners_identification_from_vision", "identifications_count", "obscured", "num_identification_disagreements", "geoprivacy", "location", "spam", "mappable", "identifications_some_agree", "place_guess", "file_url", "subtype", "play_local", "native_sound_id", "attribution", "file_id", "media_extension", "license_code", "secret_token", "hidden", "page", "repository", "latitude", "longitude"))
+    split_location <- do.call(rbind, strsplit(as.character(query_output_df$location), ","))
+    latitude <- split_location[, 1]
+    longitude <- split_location[, 2]
+    query_output_df$latitude <- latitude
+    query_output_df$longitude <- longitude
 
-      for (i in 1:nrow(names_df)) {
-        names(query_output_df)[names(query_output_df) == names_df$old[i]] <- names_df$new[i]
-      }
-
-      # Add species
-      query_output_df$species <- species
-
-      if (!all_data) {
-        query_output_df$country <- NA
-        query_output_df$date <- query_output_df$time_observed_at
-        query_output_df <- query_output_df[, c("key", "species", "date", "country", "location", "latitude", "longitude", "file_url", "repository")]
-      }
-
-      # Replace square image size to original in file_url
-      replace_image_size <- function(file_url) {
-        gsub("square", "original", file_url)
-      }
-      for (i in 1:nrow(query_output_df)) {
-        query_output_df$file_url[i] <- replace_image_size(query_output_df$file_url[i])
-      }
-
-      # Remove files that have no download link
-      query_output_df <- query_output_df[!is.na(query_output_df$file_url), ]
-
-      # Add a timestamp attribute
-      search_time <- Sys.time()
-      attr(query_output_df, "search_time") <- search_time
-      attr(query_output_df, "query_term") <- term
-      attr(query_output_df, "query_type") <- org_type
-      attr(query_output_df, "query_all_data") <- all_data
-
-      # Generate a file path by combining tempdir() with a file name
-      file_path <- file.path(tempdir(), paste0(term, ".rds"))
-
-      # Save the object to the file
-      saveRDS(query_output_df, file = file_path)
-      return(query_output_df)
+    first_id_index <- which(names(query_output_df) == "id")[1]
+    if (!is.na(first_id_index)) {
+      names(query_output_df)[first_id_index] <- "key"
     }
+
+    query_output_df$species <- species
+
+    if (!all_data) {
+      query_output_df$country <- NA
+      query_output_df$date <- query_output_df$time_observed_at
+      query_output_df <- query_output_df[, c("key", "species", "date", "country", "location", "latitude", "longitude", "file_url", "repository")]
+    }
+
+    replace_image_size <- function(file_url) {
+      gsub("square", "original", file_url)
+    }
+    for (i in 1:nrow(query_output_df)) {
+      query_output_df$file_url[i] <- replace_image_size(query_output_df$file_url[i])
+    }
+
+    query_output_df <- query_output_df[!is.na(query_output_df$file_url), ]
+
+    search_time <- Sys.time()
+    attr(query_output_df, "search_time") <- search_time
+    attr(query_output_df, "query_term") <- term
+    attr(query_output_df, "query_type") <- org_type
+    attr(query_output_df, "query_all_data") <- all_data
+
+    file_path <- file.path(tempdir(), paste0(term, ".rds"))
+    return(query_output_df)
   }
+}
+
