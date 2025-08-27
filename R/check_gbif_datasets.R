422a70ea9eb3b4db1009dd87652340a23def32a4
Marcelo <marcelo.araya@ucr.ac.cr>
2025-08-27 08:55:04 -0600

    updates on query_macaulay and vignete

--- R/check_gbif_datasets.R
+++ R/check_gbif_datasets.R
@@ -1,12 +1,12 @@
 #' Access 'gbif' dataset information
 #'
 #' \code{check_gbif_datasets} searches for dataset information from \href{https://www.gbif.org}{gbif}.
-#' @param path Directory path where the output .csv file will be saved. By default is downloaded to a temporary directory (\code{tempdir()}).
+#' @param path Directory path where the output .csv file will be saved. By default is downloaded to the current working directory (\code{"."}).
 #' @return returns dataset csv from gbif
 #' @export
 #' @name check_gbif_datasets
 #' @details This function queries for dataset info in the open-access
-#' online repository \href{https://www.gbif.org}{gbif}. It saves a csv file in a temporary directory (~42 MB). The file contains some 89000 rows of datasets to explore.
+#' online repository \href{https://www.gbif.org}{gbif}. It saves a csv file in a the supplied `path` directory (~42 MB). The file contains some 89000 rows of datasets to explore.
 #' @seealso \code{\link{check_gbif_datasets}}, \code{\link{query_observation}}, \code{\link{query_wikiaves}}, \code{\link{query_inaturalist}},
 #' @examples
 #' \dontrun{
@@ -20,9 +20,10 @@
 #' @author Marcelo Araya-Salas (\email{marcelo.araya@@ucr.ac.cr})
 #'
 
-check_gbif_datasets <- function(path = tempdir()) {
+check_gbif_datasets <- function(path = ".") {
   # Check internet connection
   response <- try(httr::GET("https://www.gbif.org"), silent = TRUE)
+
   if (inherits(response, "try-error") ||
       httr::http_error(response)) {
     stop("No connection to GBIF (check your internet connection!)")
@@ -35,34 +36,27 @@ check_gbif_datasets <- function(path = tempdir()) {
   }
 
   # Download the CSV data
-  message("Downloading CSV file ...")
+  message("Downloading GBIF dataset metadata ...")
   csv_url <- "https://api.gbif.org/v1/dataset/search/export?format=CSV&"
-  csv_response <- try(httr::GET(csv_url), silent = TRUE)
+  # csv_response <- try(httr::GET(csv_url), silent = TRUE)
+  file_name <- paste0("gbif_datasets_", format(Sys.time(), "%Y-%m-%d_%H-%M-%S"), ".csv")
+  file_name <- file.path(normalizePath(path), file_name)
 
-  if (inherits(csv_response, "try-error") ||
-      httr::http_error(csv_response)) {
+  download.file("https://api.gbif.org/v1/dataset/search/export?format=CSV&",destfile = file_name)
+
+  if (!file.exists(file_name)) {
     stop("Failed to download CSV.")
   } else {
     # Read CSV data into R
-    csv_data <- try(read.csv(text = httr::content(csv_response, "text", encoding = "UTF-8")), silent = TRUE)
+    csv_data <- try(read.csv(file_name), silent = TRUE)
     if (inherits(csv_data, "try-error")) {
-      stop("Failed to parse CSV.")
+      stop("Failed to GBIF dataset metadata")
     }
-
-    # Save the CSV file
-    file_name <- paste0("gbif_datasets-",
-                        format(Sys.time(), "%Y-%m-%d_%H-%M-%S"),
-                        ".csv")
-    file_path <- file.path(path, file_name)
-    write.csv(csv_data, file_path)
   }
 
   # Inform the user about the file
-  cli::cli_text("... edit your {.file ~/.Rprofile} file.}")
-  cli::cli_text(paste0(
-    "The CSV file (",
-    file_name,
-    ") with the GBIF datasets info can be found at ",
-    path
-  ))
+  cli::cli_text(paste0("The GBIF dataset metadata was save in ", file_name))
+
+
+  return(csv_data)
 }
