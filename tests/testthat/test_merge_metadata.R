31932caebcc17bac2f4604ee82f4b87f6d6674e0
Marcelo <marcelo.araya@ucr.ac.cr>
2025-11-10 12:06:45 -0600

    Update test_merge_metadata.R

--- tests/testthat/test_merge_metadata.R
+++ tests/testthat/test_merge_metadata.R
@@ -2,8 +2,7 @@ test_that("merge query_wikiaves and xc", {
 
 
   wa <- suwo:::vignette_metadata$h_harpyja
-  gbf <- suwo:::vignette_metadata$p_lotor
-
+  xc <- suwo:::vignette_metadata$a_hahneli
   merged_mt <- merge_metadata(wa, xc)
 
   expect_true(nrow(merged_mt) == nrow(wa) + nrow(xc))
@@ -13,8 +12,6 @@ test_that("merge query_wikiaves and xc", {
   # add a third source
   wa <- suwo:::vignette_metadata$h_harpyja
   gbf <- suwo:::vignette_metadata$p_lotor
-  xc <- suwo:::vignette_metadata$a_hahneli
-
 
   merged_mt2 <- merge_metadata(wa, xc, gbf)
 
